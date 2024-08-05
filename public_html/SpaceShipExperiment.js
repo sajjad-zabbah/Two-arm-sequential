ea2e77c09fc8713e7a458c0ad2ba0864da71aa3c
Sajjad Zabbah <sajjadzabbah@Sajjads-MacBook-Pro.local>
2024-08-05 18:05:39 +0100

    PNG to png

--- public_html/SpaceShipExperiment.js
+++ public_html/SpaceShipExperiment.js
@@ -364,7 +364,7 @@ var if_warmup = 1;
 
         CreateDiv('Stage', 'TextBoxDiv');
         var Title = '<H2 align = "center">Instructions</H2>';
-        var ThisImage = '<div align = "center"><img src="images/' + slides_set + '_Slide' + PageNum + '.PNG" alt="house" height="' + PicHeight + '" align="center"></div>';
+        var ThisImage = '<div align = "center"><img src="images/' + slides_set + '_Slide' + PageNum + '.png" alt="house" height="' + PicHeight + '" align="center"></div>';
         //        $('#TextBoxDiv').html(Title + ThisImage);
         $('#TextBoxDiv').html(ThisImage);
 
