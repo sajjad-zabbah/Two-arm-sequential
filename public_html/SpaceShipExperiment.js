a568ebd6e0573df5b627d7667be55c7e08153049
sw-2024 <zabbah121@gmail.com>
2025-04-07 18:26:43 +0100

    debug

--- public_html/SpaceShipExperiment.js
+++ public_html/SpaceShipExperiment.js
@@ -43,7 +43,7 @@ const thisWidth = screen.width * 0.9;
     
     var attentionTrialnum=2;    // After this trial, we start checking if they pay enough attention we set it to 20
     var distCrit = 10;           // If subject misses these number of trials they will be droped out 
-    var NumWarmUpTrials = 3;   // this warm up trials and should be 20 
+    var NumWarmUpTrials = 30;   // this warm up trials and should be 20 
     var NumMainTrials   = 365;  // this is main trials and should be 365
     // -----------------------
     
